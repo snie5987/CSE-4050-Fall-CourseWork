63e0bc1854370bb2003d02c9b93233b8b799cab4
snie5987 <52872433+snie5987@users.noreply.github.com>
2020-10-22 10:54:20 -0700

    Update accountCheck.js

--- accountCheck.js
+++ accountCheck.js
@@ -1,6 +1,25 @@
+var email;
+var pass;
+var error_message;
+var text;
+
+function users() {
+
+  let user = {
+    "name":["John Smith","Kathy Green", "Rebecca White"],
+    "email":["smithjohn@gmail.com","kathy_green@ymail.com","whiteReb826@gmail.com"],
+    "pass":[ "puppylove123", "kittenyarn456", "hamsterfoe789"]
+    };
+
+  var info = JSON.stringify(user);
+
+  return info;
+
+}
+
 function accountValidate(){
-  var email = document.getElementById("email").value;
-  var pass = document.getElementById("pass").value;
+  var email = document.getElementById("emailLogin").value;
+  var pass = document.getElementById("passLogin").value;
   var error_message = document.getElementById("error_message");
   
   error_message.style.padding = "10px";
@@ -17,18 +36,30 @@ function accountValidate(){
     error_message.innerHTML = text;
     return false;
   }
-  var jsonObject = {
-    "email": "johnsmith@gmail.com",
-    "pass": "doglover123"
-  };
+  
+    var accountInfo = JSON.parse(account);
 
-  const obj = JSON.parse(jsonObject);
-  error_message.innerHTML = obj;
+  for(i = 0, k = accountInfo.name.length; i < k; i++ ) {
+    if (email == accountInfo.email[i] && pass == accountInfo.pass[i]){
+      var userName = accountInfo.name[i];
+      alert ( userName + " you have successfully logged into your account");
+      return true;
+    }
+  }
+
+  alert("No user found");
+
+  text = "Please try Again";
+  error_message.innerHTML = text;
+
   return false;
-
-
-  alert("Created Account Successfully!");
-
-  return true;
 }
 
+function showPass() {
+  var x = document.getElementById("passLogin");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
